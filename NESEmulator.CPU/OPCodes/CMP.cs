32d71535748b94e669c2e71d6092a03224429ab3
WimJongeneel <wim.jongeneel1@gmail.com>
2023-03-07 20:08:14 +0100

    Enable ORA in instructions; code formatting

--- NESEmulator.CPU/OPCodes/CMP.cs
+++ NESEmulator.CPU/OPCodes/CMP.cs
@@ -10,7 +10,7 @@ public class CMP : IOPCode
 
         cpu.SetStatusFlag(CPUFlag.C, cpu.A >= cpu.FetchCache);
         cpu.SetStatusFlag(CPUFlag.Z, (result & 0x00FF) == 0x0000);
-        cpu.SetStatusFlag(CPUFlag.N, (result& 0x80) > 0);
+        cpu.SetStatusFlag(CPUFlag.N, (result & 0x80) > 0);
 
         return false;
     }
--- NESEmulator.CPU/OPCodes/CPY.cs
+++ NESEmulator.CPU/OPCodes/CPY.cs
@@ -10,7 +10,7 @@ public class CPY : IOPCode
 
         cpu.SetStatusFlag(CPUFlag.C, cpu.Y >= cpu.FetchCache);
         cpu.SetStatusFlag(CPUFlag.Z, (result & 0x00FF) == 0x0000);
-        cpu.SetStatusFlag(CPUFlag.N, (result& 0x80) > 0);
+        cpu.SetStatusFlag(CPUFlag.N, (result & 0x80) > 0);
 
         return false;
     }
